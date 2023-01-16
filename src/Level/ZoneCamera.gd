3927ade0e3e80ca0b18541884f38a8adc9167821
Nathan Foote <39596241+Nanoparty@users.noreply.github.com>
2023-01-16 01:24:43 -0600

    switching cameras working

---
+++ src/Level/ZoneCamera.gd
@@ -0,0 +1,27 @@
+extends Camera2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	if body is Player:
+		current = true
+
+
+func _on_Area2D_body_exited(body):
+	if body is Player:
+		current = false
